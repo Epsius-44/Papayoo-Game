b674a9dbc3c76db7c96b0305586a20558327a377
Louis-Ducruet <louis.ducruet@epsi.fr>
2022-04-06 10:17:28 +0200

    Add Joueur class

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // pour cin / cout
+#include <vector> // pour time()
+#include <stdlib.h>     /* srand, rand */
+#include "classe/joueur.h"
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
