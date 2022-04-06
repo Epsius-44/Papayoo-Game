00364edee8fd2e60262927ef2ebc8aed9afc65db
Louis-Ducruet <louis.ducruet@epsi.fr>
2022-04-06 14:12:28 +0200

    Fix some glitch

--- classe/joueur.h
+++ classe/joueur.h
@@ -24,7 +24,7 @@ public:
         this->cartes.insert(this->cartes.end(), carteEnPlus.begin(), carteEnPlus.end());
     }
     int unsigned lancerDe(){
-        return rand() % 4;
+        return rand() % 4 + 1;
     }
 
     virtual vector<Carte*> donneTroisCarte(){
@@ -59,7 +59,7 @@ public:
     }
     virtual Carte* jouerUneCarte(){
         unsigned int idCarteChoisi;
-        this->afficherCarte();
+        this->afficherCartes();
         cout << "Choisir une carte entre 1 et " << this->cartes.size() << " : ";
         cin >> idCarteChoisi;
         Carte* carteChoisi = this->cartes[idCarteChoisi - 1];
