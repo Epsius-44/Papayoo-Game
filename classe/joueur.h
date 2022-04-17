59f349b0ae035d6794cbf2ed2ce72de59a7a23fb
livrophage <tom.chanson@epsi.fr>
2022-04-17 09:31:21 +0200

    ajout d'une fonction retournant la liste des index des cartes qui peuvent être joué par un joueur

--- classe/joueur.h
+++ classe/joueur.h
@@ -1,68 +1,91 @@
-class Joueur{
+class Joueur {
 private:
     string nom;
 protected:
     int unsigned points;
-    vector<Carte*> cartes;
+    vector<Carte *> cartes;
 public:
-    Joueur(string nom){
+    Joueur(string nom) {
         this->nom = nom;
         this->points = 0;
         this->cartes = {};
     }
 
-    void afficherCartes(){
-        for (int i = 0; i < this->cartes.size() ; ++i) {
+    void afficherCartes() {
+        for (int i = 0; i < this->cartes.size(); ++i) {
             cout << i + 1 << " | ";
             this->cartes[i]->afficherCarte();
         }
     }
 
-    void recoisCartes(vector<Carte*> carteEnPlus){
+    void recoisCartes(vector<Carte *> carteEnPlus) {
         this->cartes.insert(this->cartes.end(), carteEnPlus.begin(), carteEnPlus.end());
     }
-    int unsigned lancerDe(){
+
+    int unsigned lancerDe() {
         return rand() % 4 + 1;
     }
 
-    virtual vector<Carte*> donneTroisCarte(){
+    virtual vector<Carte *> donneTroisCarte() {
         return {nullptr};
     };
-    virtual Carte* jouerUneCarte(){
+
+    virtual Carte *jouerUneCarte() {
         return nullptr;
     };
 
-    string getNom(){
+    string getNom() {
         return this->nom;
     }
-    int unsigned getPoints(){
+
+    int unsigned getPoints() {
         return this->points;
     }
-    void addPoints(int unsigned points){
+
+    void addPoints(int unsigned points) {
         this->points += points;
     }
-    vector<Carte*> getCartes(){
+
+    vector<Carte *> getCartes() {
         return this->cartes;
     }
-    void setCartes(vector<Carte*> cartes){
+
+    void setCartes(vector<Carte *> cartes) {
         this->cartes = cartes;
     }
-};
 
-class Humain : public Joueur{
+    vector<int> cartesDispo(int couleurCarteJouer=0) {
+        vector<int> indexCartesDispo = {}; //vector avec la liste des index des cartes qui peuvent être joué par le joueur (bot ou humain)
+        for (int i = 0; i < this->cartes.size(); i++) { //boucle pour chaque carte présente dans le jeu du joueur
+            if (couleurCarteJouer == 0 or this->cartes[i]->getCouleur()==couleurCarteJouer) { //si aucune carte n'a été joué ou que la couleur de la carte correspond à la première carte joué pour le pli
+                indexCartesDispo.push_back(i); //ajouter index de la carte à la liste des cartes disponibles
+            }
+        }
+        if (indexCartesDispo.size()==0){ //si le jeu du joueur ne contient aucune carte avec la même couleur que la première carte joué pour le pli
+            for (int i = 0; i < this->cartes.size(); i++) {
+                    indexCartesDispo.push_back(i); //ajout de chaque carte du joueur dans la liste des cartes dispo
+            }
+        }
+        return indexCartesDispo; //renvoi de la liste des index correspondant aux cartes que le joueur peut jouer
+    }
+}
+
+class Humain : public Joueur {
 public:
     Humain(string nom) : Joueur(nom) {}
-    virtual vector<Carte*> donneTroisCarte(){
+
+    virtual vector<Carte *> donneTroisCarte() {
         //shuffle(this->cartes.begin(), this->cartes.end(), default_random_engine(666));
         // TODO Remove card
-        return vector<Carte*>(this->cartes.begin(), this->cartes.begin());
+        return vector<Carte *>(this->cartes.begin(), this->cartes.begin());
     }
-    virtual Carte* jouerUneCarte(){
+
+    virtual Carte *jouerUneCarte() {
         unsigned int idCarteChoisi;
         this->afficherCartes();
         cout << "Choisir une carte entre 1 et " << this->cartes.size() << " : ";
         cin >> idCarteChoisi;
-        Carte* carteChoisi = this->cartes[idCarteChoisi - 1];
+        Carte *carteChoisi = this->cartes[idCarteChoisi - 1];
         cout << "Le joueur " << this->getNom() << " joue : ";
         carteChoisi->afficherCarte();
         this->cartes.erase(this->cartes.begin() + idCarteChoisi - 1);
@@ -70,14 +93,16 @@ public:
     }
 };
 
-class Bot : public Joueur{
+class Bot : public Joueur {
 public:
     Bot(string nom) : Joueur(nom) {}
-    virtual vector<Carte*> donneTroisCarte(){
+
+    virtual vector<Carte *> donneTroisCarte() {
         shuffle(this->cartes.begin(), this->cartes.end(), default_random_engine(666));
-        return vector<Carte*>(this->cartes.begin(), this->cartes.begin()+3);
+        return vector<Carte *>(this->cartes.begin(), this->cartes.begin() + 3);
     }
-    virtual Carte* jouerUneCarte(){
+
+    virtual Carte *jouerUneCarte() {
         return this->cartes[rand() % this->cartes.size()];
     }
 };
